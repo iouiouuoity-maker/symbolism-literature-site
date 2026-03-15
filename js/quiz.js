d8bc0def95017eedfb6aa0a2eb84652e119321b4
iouiouuoity-maker <iouiouuoity@gmail.com>
2026-03-16 01:50:18 +0500

    Update quiz.js

--- js/quiz.js
+++ js/quiz.js
@@ -1,62 +1,83 @@
-const questions=[
+const questions = [
 
 {
-q:"Шыңырау символы нені білдіреді?",
-a:["Тағдыр","Табиғат","Жол"],
-c:0
+question:"«Шыңырау» символы нені білдіреді?",
+answers:["Тағдыр","Табиғат","Жол","Достық"],
+correct:0
 },
 
 {
-q:"Сантьяго кім?",
-a:["Балықшы","Құдық қазушы","Жазушы"],
-c:0
+question:"Сантьяго кім?",
+answers:["Құдық қазушы","Балықшы","Жазушы","Суретші"],
+correct:1
 },
 
 {
-q:"Теңіз символы нені білдіреді?",
-a:["Өмір","Уақыт","Достық"],
-c:0
+question:"Теңіз символы нені білдіреді?",
+answers:["Өмір кеңістігі","Ауа","Уақыт","Тыныштық"],
+correct:0
+},
+
+{
+question:"Үлкен балық нені білдіреді?",
+answers:["Мақсат пен күрес","Достық","Табиғат","Жол"],
+correct:0
+},
+
+{
+question:"Еңсеп қандай кейіпкер?",
+answers:["Жауынгер","Құдық қазушы","Саяхатшы","Жазушы"],
+correct:1
 }
 
 ]
 
-let current
+let currentQuestion
 let score=0
 
-function load(){
+function loadQuestion(){
 
-current=Math.floor(Math.random()*questions.length)
+currentQuestion=Math.floor(Math.random()*questions.length)
 
-document.getElementById("question").innerText=questions[current].q
+document.getElementById("question").innerText=
+questions[currentQuestion].question
 
-let html=""
+let answersHTML=""
 
-questions[current].a.forEach((x,i)=>{
-html+=`<button onclick="check(${i})">${x}</button>`
+questions[currentQuestion].answers.forEach((answer,index)=>{
+
+answersHTML+=`<button onclick="checkAnswer(${index})">${answer}</button>`
+
 })
 
-document.getElementById("answers").innerHTML=html
+document.getElementById("answers").innerHTML=answersHTML
 
 }
 
-function check(i){
+function checkAnswer(answer){
 
-if(i===questions[current].c){
+if(answer===questions[currentQuestion].correct){
+
 score++
-alert("Дұрыс")
+
+alert("Дұрыс!")
+
 }
 
 else{
-alert("Қате")
-}
+
+alert("Қате!")
 
 }
 
-function next(){
+}
 
-load()
+function nextQuestion(){
+
+loadQuestion()
+
 document.getElementById("score").innerText="Ұпай: "+score
 
 }
 
-load()
+loadQuestion()
